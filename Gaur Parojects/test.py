80bc000398ca62022c32f563b79568d13480f26d
HaramiFunker <namami2011@gmail.com>
2020-10-22 03:08:40 +0530

    Added Gaur projects

---
+++ Gaur Parojects/test.py
@@ -0,0 +1,8 @@
+def main():
+    n, h, y1, y2, l = list(map(int, input().split()))
+    for i in range(n):
+        type, x = list(map(int, input("Enter type and height of barrier: ").split()))
+
+t = int(input())
+for i in range(t):
+    main()
